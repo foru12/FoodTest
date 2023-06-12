c5fa37723a789e40acb7c0cd008f74d4a9fe26ba
Thorin Oakenshield <kostya1705@gmail.com>
2023-06-12 12:34:41 +0300

    add bottomNavigation

---
+++ app/src/main/java/com/example/foodtest/View/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.foodtest.View
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.foodtest.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+
+
+
+
+
+    }
+}
